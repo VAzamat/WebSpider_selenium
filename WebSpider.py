1bafccdbe09912b6fbeed27c0a1a9c21763da9f7
Valeev Azamat <vazamat@gmail.com>
2026-03-30 18:38:51 +0300

    firefox with profile and target/save directory

--- WebSpider.py
+++ WebSpider.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 import os
+import time
 
 from bs4 import BeautifulSoup
 import requests
 
 import logging
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 main_logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -15,6 +19,18 @@ logging.basicConfig(
 
 main_logger.info("запускаю основную программу")
 
+main_logger.info("запускаю графический просмотр через браузер")
+firefox_options = webdriver.FirefoxOptions()
+firefox_options.add_argument('-profile')
+firefox_options.add_argument(os.environ.get('FirefoxProfilePath'))
+
+firefox_options.set_preference("browser.download.dir", os.environ.get('FirefoxDestDir'))
+firefox_options.set_preference("browser.download.folderList", 2)
+firefox_options.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/html, application/xhtml+xml")
+
+browser = webdriver.Firefox(firefox_options)
+time.sleep(1)
+browser.close()
 
 def getAllLinks(url, include_only_parent=True):
     filename = os.path.basename(url)
